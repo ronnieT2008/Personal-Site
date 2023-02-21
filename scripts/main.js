2e3baf43ed9bd62b3e68cd6675feb5147103c902
ronniet2008 <tisronik2008@gmail.com>
2023-02-20 19:15:46 -0500

    cleaned navbar scroll CSS

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,17 @@
 $(window).scroll(function(){
     let scroll = $(window).scrollTop();
     if (scroll > 100) {
-        $(".navbar").css("background" , "#343a40");
-        $(".nav-link").css("color" , "#fff");
-        $(".active").css("color" , "#ffc107");
-        $(".bi").css("color" , "#fff");
+        $(".navbar").addClass("scroll");
+        // $(".navbar").css("background" , "#343a40");
+        // $(".nav-link").css("color" , "#fff");
+        // $(".active").css("color" , "#ffc107");
+        // $(".bi").css("color" , "#fff");
     }
     else{
-        $(".navbar").css("background" , "transparent");  
-        $(".nav-link").css("color" , "#000");
-        $(".active").css("color" , "#fff");	
-        $(".bi").css("color" , "#000");
+        $(".navbar").removeClass("scroll");
+        // $(".navbar").css("background" , "");  
+        // $(".nav-link").css("color" , "");
+        // $(".active").css("color" , "");	
+        // $(".bi").css("color" , "");
     }
 })
